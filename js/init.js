105de55a55237f1e1ba66ed2d218b2f19ddb046c
Natacha Aviles <Natacha@Imaginexs-MBP.home>
2017-07-14 00:01:40 -0400

    vistas agregadas

--- js/init.js
+++ js/init.js
@@ -1,41 +1,41 @@
 $(document).ready(function(){
 	$('.button-collapse').sideNav();
 
-	 $('select').material_select();
+	$('select').material_select();
 
-	 $('.datepicker').pickadate({
+	$('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
-	});
+});
 
 	onsubmit();
-});        
+	searchdates();
+});  
 
-	function validate(){
-   		let especialidad = $( "#especialidad option:selected" ).text();
-   		let fecha = $('#appointment-date').val();
 
-   		if(especialidad == null || especialidad == ''){
-   			alert('Debe elegir una especialidad para consultar');
-   		}else if(fecha == null || fecha == ''){
-   			alert('Debe elegir fecha para consultar')
-   		}else {
-   			searchdates();
-   		}
+function validate(){
+	let especialidad = $("#especialidad option:selected" ).text();
+	let fecha = $('#appointment-date').val();
+
+	if(especialidad == null || especialidad == ''){
+		alert('Debe elegir una especialidad para consultar');
+	}else if(fecha == null || fecha == ''){
+		alert('Debe elegir fecha para consultar')
+	}else {
+		searchdates();
 	}
+}
 
-	function searchdates(){
-		let dates = get_dates();
-		let mainsection = $('#main-section');
-		console.log(dates);
+function searchdates(){
+	let dates = get_dates();
+	let container = $('#take_appointment');
+}
 
-		$.each(dates, function(index,hours ) {
-			 mainsection.append($('<div class="col m3 s3 hour-card"> + (hours[index].day) + (hours[index].hour) + <div>'));
-		});
-	}
+function onsubmit(){
+	$('#searchAvailability').click(function(event) {
+		$('#searchAvailability').addClass('hidden');
+		validate();
+		$('#appointments').removeClass('hidden');
 
-	function onsubmit(){
-		$('#search').click(function(event) {
-			validate();
-		});
-	}
+	});
+}
